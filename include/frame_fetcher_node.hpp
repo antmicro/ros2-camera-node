1eb5e71990e2b5a32d9c3ac860c4116eb42c405f
Marcelina Oset <moset@internships.antmicro.com>
2023-03-23 11:12:27 +0100

    [#41079] include: frame_fetcher_node: Added missing docstrings

--- include/frame_fetcher_node.hpp
+++ include/frame_fetcher_node.hpp
@@ -5,9 +5,23 @@
 
 namespace camera_node
 {
+
+/**
+ * ROS 2 Composite node.
+ * This node is capable of displaying images received from camera_frame topic using OpenCV.
+ *
+ * * Available topics include:
+ * - camera_frame : Stream of frames from a camera in CameraFrame message format.
+ */
 class FrameFetcherNode : public rclcpp::Node
 {
+    /**
+     * @brief A callback method that is to be executed every time this Node receive
+     * something that is posted to a CameraFrame topic.
+     */
     void frame_fetcher_callback(const sensor_msgs::msg::Image &message) const;
+
+    /// @brief Subscriber for camera_frame topic.
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr frame_fetcher_sub;
 
 public:
